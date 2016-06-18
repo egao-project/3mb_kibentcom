7f1ca5bf5c4fed699da2ebbabe0ffd50169924a0
ushikit <ushikit@egao-inc.co.jp>
2016-06-18 20:24:37 +0900

    詳細画面できた。たぶん

--- railsapp/app/controllers/list_controller.rb
+++ railsapp/app/controllers/list_controller.rb
@@ -4,4 +4,18 @@ class ListController < ApplicationController
   	@events = Event.all
   end
 
+  # GET /events/1
+  # GET /events/1.json
+  def show
+  	@event = Event.find(params[:id])
+  end
+
+  def add_entrant
+  	entrant = Entrant.new()
+  	entrant.event_id = params[:event_id]
+  	entrant.company_id = params[:company_id]
+  	#entrant.user_id = 0
+  	entrant.save
+  end
+
 end
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   resources :events
-  resources :list
+  resources :list do
+    member do
+      get 'show'
+      get 'index'
+    end
+
+    collection do
+      get 'add_entrant'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

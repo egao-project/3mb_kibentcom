1694ea9ae23c7c63a12887a6d32009d957fb7544
ushikit <ushikit@egao-inc.co.jp>
2016-08-19 18:20:10 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-belongs_to :entrant
+#belongs_to :entrant
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
